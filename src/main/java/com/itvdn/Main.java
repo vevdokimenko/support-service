424bffda02a25cbef94cb8332528650620c3c006
vevdokimenko <evdokimenko@hotmail.com>
2022-01-19 23:57:18 +0200

    Split command and id. Some commands released

--- src/main/java/com/itvdn/Main.java
+++ src/main/java/com/itvdn/Main.java
@@ -8,17 +8,29 @@ public class Main {
     public static void main(String[] args) {
         Auth auth = new Auth();
         MyQuery query = new MyQuery();
-
         String command = "";
+
         while (!command.equals("exit")) {
             System.out.println("Type command below, or \"exit\" to exit program");
             command = sc.nextLine();
+            String id = command.replaceAll("[^\\d+]","");
+            command = command.replaceAll("\\d+", "");
 
             switch (command) {
                 case "exit":
                     break;
                 case "fetch_all_users":
+                    System.out.println(id);
                     query.fetchAllUsers(auth.getActiveUser(), command);
+                    break;
+                case "fetch_all_incidents":
+                    query.fetchAllIncidents(auth.getActiveUser(), command);
+                    break;
+                case "fetch_all_active_incidents":
+                    query.fetchAllActiveIncidents(auth.getActiveUser(), command);
+                    break;
+                case "fetch_user_by_{}":
+                    query.fetchUserById(auth.getActiveUser(), command, id);
                     break;
                 default:
                     System.err.println("Unknown command.");
--- src/main/java/com/itvdn/MyQuery.java
+++ src/main/java/com/itvdn/MyQuery.java
@@ -1,13 +1,15 @@
 package com.itvdn;
 
+import com.itvdn.entity.IncidentEntity;
 import com.itvdn.entity.UserEntity;
+import com.itvdn.helper.IncidentHelper;
 import com.itvdn.helper.UserHelper;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MyQuery {
-    private List<Permission> permissions = Arrays.asList(
+    private final List<Permission> permissions = Arrays.asList(
             new Permission("fetch_all_users", "SUPER_ADMIN"),
             new Permission("fetch_all_users", "ADMIN"),
 
@@ -17,25 +19,25 @@ public class MyQuery {
             new Permission("fetch_all_active_incidents", "SUPER_ADMIN"),
             new Permission("fetch_all_active_incidents", "ADMIN"),
 
-            new Permission("fetch_user_by_id", "SUPER_ADMIN"),
-            new Permission("fetch_user_by_id", "ADMIN"),
+            new Permission("fetch_user_by_{}", "SUPER_ADMIN"),
+            new Permission("fetch_user_by_{}", "ADMIN"),
 
             new Permission("add_user", "SUPER_ADMIN"),
             new Permission("add_user", "ADMIN"),
 
-            new Permission("update_user_id", "SUPER_ADMIN"),
-            new Permission("update_user_id", "ADMIN"),
+            new Permission("update_user_{}", "SUPER_ADMIN"),
+            new Permission("update_user_{}", "ADMIN"),
 
-            new Permission("delete_user_id", "SUPER_ADMIN"),
-            new Permission("delete_user_id", "ADMIN"),
+            new Permission("delete_user_{}", "SUPER_ADMIN"),
+            new Permission("delete_user_{}", "ADMIN"),
 
-            new Permission("subscribe_service_id", "SUPER_ADMIN"),
-            new Permission("subscribe_service_id", "ADMIN"),
-            new Permission("subscribe_service_id", "USER"),
+            new Permission("subscribe_service_{}", "SUPER_ADMIN"),
+            new Permission("subscribe_service_{}", "ADMIN"),
+            new Permission("subscribe_service_{}", "USER"),
 
-            new Permission("unsubscribe_service_id", "SUPER_ADMIN"),
-            new Permission("unsubscribe_service_id", "ADMIN"),
-            new Permission("unsubscribe_service_id", "USER"),
+            new Permission("unsubscribe_service_{}", "SUPER_ADMIN"),
+            new Permission("unsubscribe_service_{}", "ADMIN"),
+            new Permission("unsubscribe_service_{}", "USER"),
 
             new Permission("create_incident", "SUPER_ADMIN"),
             new Permission("create_incident", "ADMIN"),
@@ -45,10 +47,14 @@ public class MyQuery {
             new Permission("close_incident", "ADMIN")
     );
 
+    private boolean isAllowed(UserEntity user, String command) {
+        return permissions.contains(new Permission(command, user.getUserRole().getRoleName()));
+    }
+
     public void fetchAllUsers(UserEntity user, String command) {
         if (isAllowed(user, command)) {
             UserHelper userHelper = new UserHelper();
-            for (UserEntity item : userHelper.getUserList()){
+            for (UserEntity item : userHelper.getUserList()) {
                 System.out.println(item);
             }
         } else {
@@ -56,7 +62,21 @@ public class MyQuery {
         }
     }
 
-    private boolean isAllowed(UserEntity user, String command){
-        return permissions.contains(new Permission(command, user.getUserRole().getRoleName()));
+    public void fetchAllIncidents(UserEntity activeUser, String command) {
+        for (IncidentEntity item : new IncidentHelper().getIncidentList()){
+            System.out.println(item);
+        }
+    }
+
+    public void fetchAllActiveIncidents(UserEntity activeUser, String command) {
+
+    }
+
+    public void fetchUserById(UserEntity activeUser, String command, String id) {
+        if (isAllowed(activeUser, command)) {
+            System.out.println(new UserHelper().getUserById(Long.parseLong(id)));
+        } else {
+            System.err.println("This command is not allowed for you");
+        }
     }
 }
