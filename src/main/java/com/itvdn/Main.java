93738f703dce28b472495934c27b6d429c9d253d
vevdokimenko <evdokimenko@hotmail.com>
2022-01-19 23:17:16 +0200

    Permissions added

--- src/main/java/com/itvdn/Main.java
+++ src/main/java/com/itvdn/Main.java
@@ -3,7 +3,7 @@ package com.itvdn;
 import java.util.Scanner;
 
 public class Main {
-    private static Scanner sc = new Scanner(System.in);
+    private static final Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
         Auth auth = new Auth();
@@ -25,9 +25,5 @@ public class Main {
                     break;
             }
         }
-    }
-
-    private static void fetchAllUsers() {
-        System.out.println("fetch_all_users");
     }
 }
